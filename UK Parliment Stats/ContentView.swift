63bb4cf3822ccad2c05d95bd59453b53adec3abf
Henry Hudson <henry.hudson@digitalworkroom.co.uk>
2020-12-11 21:54:33 +0000

    Initial Commit

---
+++ UK Parliment Stats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UK Parliment Stats
+//
+//  Created by Henry Hudson on 11/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UK Parliment Stats/UK_Parliment_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UK_Parliment_StatsApp.swift
+//  UK Parliment Stats
+//
+//  Created by Henry Hudson on 11/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct UK_Parliment_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
